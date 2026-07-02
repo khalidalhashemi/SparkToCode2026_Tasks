65b7f859636d0f2d6453a56999f858e2b9171dd1
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-02 23:47:12 +0400

    Task 1: Personal Info Card

---
+++ FundamentalTask1Solution/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FundamentalTask1Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1: Personal Info Card
+
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Your Height (in meters): ");
+            double height = double.Parse(Console.ReadLine());
+
+            bool Student = age <= 18;
+
+            Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height + ", isStudent: " + Student);
+
+        }
+    }
+}
