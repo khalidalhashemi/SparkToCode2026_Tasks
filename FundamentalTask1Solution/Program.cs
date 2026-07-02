601c8a793550b96589f7fc305cf25c82a4916d04
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-02 23:49:46 +0400

    Task 2: Rectangle Calculator

--- FundamentalTask1Solution/Program.cs
+++ FundamentalTask1Solution/Program.cs
@@ -6,18 +6,36 @@
         {
             // Task 1: Personal Info Card
 
-            Console.Write("Enter Your Name: ");
-            string name = Console.ReadLine();
+            //Console.Write("Enter Your Name: ");
+            //string name = Console.ReadLine();
 
-            Console.Write("Enter Your Age: ");
-            int age = int.Parse(Console.ReadLine());
+            //Console.Write("Enter Your Age: ");
+            //int age = int.Parse(Console.ReadLine());
 
-            Console.Write("Enter Your Height (in meters): ");
-            double height = double.Parse(Console.ReadLine());
+            //Console.Write("Enter Your Height (in meters): ");
+            //double height = double.Parse(Console.ReadLine());
 
-            bool Student = age <= 18;
+            //bool Student = age <= 18;
 
-            Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height + ", isStudent: " + Student);
+            //Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height + ", isStudent: " + Student);
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///
+
+
+            // Task 2: Rectangle Calculator
+
+            Console.Write("Enter the length of the rectangle: ");
+            float length = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the width of the rectangle: ");
+            float width = float.Parse(Console.ReadLine());
+
+            float Area = length * width;
+            float Perimeter = 2 * (length + width);
+
+            Console.WriteLine("Area of the rectangle: " + Area);
+            Console.WriteLine("Perimeter of the rectangle: " + Perimeter);
 
         }
     }
