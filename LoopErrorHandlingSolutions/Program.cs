29d6550fd4574cf78abe61fd3f14ab4ccbc922e9
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-03 20:07:03 +0400

    Task 1: Countdown Timer

--- LoopErrorHandlingSolutions/Program.cs
+++ LoopErrorHandlingSolutions/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Loop & Error Handling Task
+
+            // Task 1: Countdown Timer
+
+            int CountDownNum = 0;
+            int countDown;
+
+            Console.WriteLine("Enter a number to countdown from: ");
+            CountDownNum = int.Parse(Console.ReadLine());
+
+            for(countDown = CountDownNum; countDown >= 1; countDown--)
+            {
+                Console.WriteLine(countDown);
+            }
+
+            Console.WriteLine("Liftoff!");
+            
+            ////////////////////////////////////////////////////////////////////////////////////////
         }
     }
 }
