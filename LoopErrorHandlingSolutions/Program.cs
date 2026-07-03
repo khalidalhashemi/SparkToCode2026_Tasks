28fa575f62af6e58ceae4094a86462f50ceea5f8
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-04 00:46:14 +0400

    Task 2: Sum of Numbers 1 to N

--- LoopErrorHandlingSolutions/Program.cs
+++ LoopErrorHandlingSolutions/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace LoopErrorHandlingSolutions
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+
+namespace LoopErrorHandlingSolutions
 {
     internal class Program
     {
@@ -8,20 +11,37 @@
 
             // Task 1: Countdown Timer
 
-            int CountDownNum = 0;
-            int countDown;
+            //int CountDownNum = 0;
+            //int countDown;
 
-            Console.WriteLine("Enter a number to countdown from: ");
-            CountDownNum = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a number to countdown from: ");
+            //CountDownNum = int.Parse(Console.ReadLine());
 
-            for(countDown = CountDownNum; countDown >= 1; countDown--)
+            //for(countDown = CountDownNum; countDown >= 1; countDown--)
+            //{
+            //    Console.WriteLine(countDown);
+            //}
+
+            //Console.WriteLine("Liftoff!");
+
+            ////////////////////////////////////////////////////////////////////////////////////////
+            ///
+
+
+            // Task 2: Sum of Numbers 1 to N
+
+            int n = 1;
+            int sum = 0;
+
+            Console.WriteLine("Enter a positive whole number: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine(countDown);
+                sum = sum + i;
             }
 
-            Console.WriteLine("Liftoff!");
-            
-            ////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("The sum from 1 to " + n + " is " + sum);
         }
     }
 }
