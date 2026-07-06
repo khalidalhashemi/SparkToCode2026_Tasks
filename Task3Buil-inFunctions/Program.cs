2f48d28c385a4a5a1b4a5314c23fbe207b7b80aa
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-06 13:38:13 +0400

    Task 1: Absolute Difference

--- Task3Buil-inFunctions/Program.cs
+++ Task3Buil-inFunctions/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Task3Buil_inFunctions
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Task3Buil_inFunctions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1: Absolute Difference
+
+            float sub;
+
+            Console.WriteLine("Enter first number: ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            sub = num2 - num1;
+
+            int Absolute = (int)Math.Abs(sub);
+
+            Console.WriteLine("The result is: " + Absolute);
+
         }
     }
 }
