52eedcd96167470f720f5bb7d724c0f9782b54d3
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-06 13:46:40 +0400

    Task 2: Power & Root Explorer

--- Task3Buil-inFunctions/Program.cs
+++ Task3Buil-inFunctions/Program.cs
@@ -8,19 +8,37 @@ namespace Task3Buil_inFunctions
         {
             // Task 1: Absolute Difference
 
-            float sub;
+            //float sub;
 
-            Console.WriteLine("Enter first number: ");
-            float num1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter first number: ");
+            //float num1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter second number: ");
-            float num2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter second number: ");
+            //float num2 = float.Parse(Console.ReadLine());
 
-            sub = num2 - num1;
+            //sub = num2 - num1;
 
-            int Absolute = (int)Math.Abs(sub);
+            //int Absolute = (int)Math.Abs(sub);
 
-            Console.WriteLine("The result is: " + Absolute);
+            //Console.WriteLine("The result is: " + Absolute);
+
+            ///////////////////////////////////////////////////////////////////////////
+            ///
+
+
+            // Task 2: Power & Root Explorer
+
+            int number;
+
+            Console.WriteLine("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            double Power = Math.Pow(number, 2);
+
+            double squareRoot = Math.Sqrt(number);
+
+            Console.WriteLine("Your power of 2 is: " + Power);
+            Console.WriteLine("Your Square Root is: " + squareRoot);
 
         }
     }
