b35219ec1fbb74c48996ffd6329261c392a8c76d
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-07 15:07:03 +0400

    Task 2: Square Number Function

--- CsharpSession4Task/Program.cs
+++ CsharpSession4Task/Program.cs
@@ -3,19 +3,46 @@
     internal class Program
     {
 
-        public static void PrintWelcome(string name)
+        //public static void PrintWelcome(string name)
+        //{
+        //    Console.WriteLine("Welcome to Csharp Program " + name);
+        //}
+
+        public static int Square(int number)
         {
-            Console.WriteLine("Welcome to Csharp Program " + name);
+            int result;
+
+            result = (int)Math.Sqrt(number);
+
+            return  result;
         }
+
         static void Main(string[] args)
         {
 
             // Task 1: Personalized Welcome Function
 
-            Console.WriteLine("Enter your name: ");
-            string name = Console.ReadLine();
+            //Console.WriteLine("Enter your name: ");
+            //string name = Console.ReadLine();
 
-            PrintWelcome(name);
+            //PrintWelcome(name);
+
+            /////////////////////////////////////////////////////////
+            ///
+
+
+            // Task 2: Square Number Function
+
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int result;
+
+            result = Square(number);
+
+            Console.WriteLine("Your result: " + result);
+
+
 
         }
     }
