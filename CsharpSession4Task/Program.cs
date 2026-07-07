b52bd6d5270a1575be7f98a6e4d6fcd3bf7f7349
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-07 14:29:06 +0400

    Task 1: Personalized Welcome Function

--- CsharpSession4Task/Program.cs
+++ CsharpSession4Task/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+
+        public static void PrintWelcome(string name)
+        {
+            Console.WriteLine("Welcome to Csharp Program " + name);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Task 1: Personalized Welcome Function
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            PrintWelcome(name);
+
         }
     }
 }
