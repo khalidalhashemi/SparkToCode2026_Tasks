65e70cad7c578056e7de6c627b1e68dfd10276b3
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-08 16:08:53 +0400

    Task 1: Fixed Grades Array

--- SolutionTask5Collections/Program.cs
+++ SolutionTask5Collections/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Task 1: Fixed Grades Array
+
+            int[] studentGrade = new int[5];
+            studentGrade[0] = 0;
+            studentGrade[1] = 0;
+            studentGrade[2] = 0;
+            studentGrade[3] = 0;
+            studentGrade[4] = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter your grades: ");
+                int grade = int.Parse(Console.ReadLine());
+
+                studentGrade[i] = grade;
+
+            }
+
+            Console.WriteLine("Your grades: ");
+            foreach (int grade in studentGrade)
+            {
+                Console.WriteLine(grade);
+            }
+
+            //////////////////////////////////////////////////////////////
+
+            
         }
     }
 }
