b00389dbad3b9f5fa6c5e24585fc694a53944a7a
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-08 21:16:11 +0400

    Task 3: Browsing History Stack

--- SolutionTask5Collections/Program.cs
+++ SolutionTask5Collections/Program.cs
@@ -35,23 +35,61 @@
 
             // Task 2:  Dynamic To-Do List
 
-            List<string> ToDoList = new List<string>();
+            //List<string> ToDoList = new List<string>();
 
-            for (int i = 0; i < 5; i++)
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Enter todays tasks: ");
+            //    string taskName = Console.ReadLine();
+
+            //    ToDoList.Add(taskName);
+
+            //}
+
+            //foreach (string taskName in ToDoList)
+            //{
+            //    Console.WriteLine("Your tasks for today: " + taskName);
+            //}
+
+            //////////////////////////////////////////////////////////////
+            ///
+
+
+            // Task 3: Browsing History Stack
+
+            string show;
+            string history;
+
+            Console.WriteLine("Enter first website: ");
+            string url1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second website: ");
+            string url2 = Console.ReadLine();
+
+            Console.WriteLine("Enter third website: ");
+            string url3 = Console.ReadLine();
+
+            Stack<string> browserHistory = new Stack<string>();
+            browserHistory.Push(url1);
+            browserHistory.Push(url2);
+            browserHistory.Push(url3);
+
+            show = browserHistory.Peek();
+
+            history = browserHistory.Pop();
+
+            Console.WriteLine("Your browsing history: ");
+
+            foreach (string url in browserHistory)
             {
-                Console.WriteLine("Enter todays tasks: ");
-                string taskName = Console.ReadLine();
-
-                ToDoList.Add(taskName);
-
+                Console.WriteLine(url);
             }
 
-            foreach (string taskName in ToDoList)
-            {
-                Console.WriteLine("Your tasks for today: " + taskName);
-            }
+            //////////////////////////////////////////////////////////////
+            ///
 
 
+   
 
 
 
