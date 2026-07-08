e6a6fd5fd531ed624e2a6933bbf84a9828efd077
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-08 20:58:49 +0400

    Task 2:  Dynamic To-Do List

--- SolutionTask5Collections/Program.cs
+++ SolutionTask5Collections/Program.cs
@@ -7,31 +7,55 @@
 
             // Task 1: Fixed Grades Array
 
-            int[] studentGrade = new int[5];
-            studentGrade[0] = 0;
-            studentGrade[1] = 0;
-            studentGrade[2] = 0;
-            studentGrade[3] = 0;
-            studentGrade[4] = 0;
+            //int[] studentGrade = new int[5];
+            //studentGrade[0] = 0;
+            //studentGrade[1] = 0;
+            //studentGrade[2] = 0;
+            //studentGrade[3] = 0;
+            //studentGrade[4] = 0;
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Enter your grades: ");
+            //    int grade = int.Parse(Console.ReadLine());
+
+            //    studentGrade[i] = grade;
+
+            //}
+
+            //Console.WriteLine("Your grades: ");
+            //foreach (int grade in studentGrade)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //////////////////////////////////////////////////////////////
+            ///
+
+
+            // Task 2:  Dynamic To-Do List
+
+            List<string> ToDoList = new List<string>();
 
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("Enter your grades: ");
-                int grade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter todays tasks: ");
+                string taskName = Console.ReadLine();
 
-                studentGrade[i] = grade;
+                ToDoList.Add(taskName);
 
             }
 
-            Console.WriteLine("Your grades: ");
-            foreach (int grade in studentGrade)
+            foreach (string taskName in ToDoList)
             {
-                Console.WriteLine(grade);
+                Console.WriteLine("Your tasks for today: " + taskName);
             }
 
-            //////////////////////////////////////////////////////////////
 
-            
+
+
+
+
         }
     }
 }
