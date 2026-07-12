3822e10b2cb924d5f469d3f8b3e8e240e1781881
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-12 21:19:53 +0400

    Add classes

--- OppPart1Task/Program.cs
+++ OppPart1Task/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace OppPart1Task
 {
-    internal class Program
+
+    public class BankAccount
+    {
+        public int AccountNumber { get; set; }
+
+        public string HolderName { get; set; }
+
+        public double Balance { get; set; }
+
+    }
+
+
+    public class Student
+    {
+        public int Grade { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        private string Email { get; set; }
+
+        int Age { get; set; }
+    }
+
+
+    public class Product
+    {
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public int StackQuantity { get; set; }
+    }
+
+
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
