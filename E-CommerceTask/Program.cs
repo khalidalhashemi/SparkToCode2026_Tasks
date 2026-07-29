4030c4a0c2bcbc77d3ba1fe145fbd9c19baa2f95
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-29 19:33:49 +0400

    Add ERD

---
+++ E-CommerceTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_CommerceTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
